18d0a9093e5436dd1fdcf7c7756fa741015dab02
Rafay Chohan <abrkchohan@gmail.com>
2024-12-04 16:19:08 +0500

    Scene Changing

--- Assets/Scripts/TestInteraction.cs
+++ Assets/Scripts/TestInteraction.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TestInteraction : MonoBehaviour, IInteractable
 {
     public void Interact()
     {
-        Debug.Log("Isbah is Gay");
+        //change scene to the next scene
+        Debug.Log("Interacting with the object");
+        changeScene();
+    }
+    private void changeScene()
+    {
+        //change scene to the next scene
+        Debug.Log("Changing scene");
+        SceneManager.LoadScene(1);
     }
 }
 
